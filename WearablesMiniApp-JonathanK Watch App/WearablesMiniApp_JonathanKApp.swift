36da8ec0f56d2e45538ae2483a024c397983a3ff
MrKearns <jkearns631@students.mchenry.edu>
2023-02-16 13:02:44 -0600

    Initial Commit

---
+++ WearablesMiniApp-JonathanK Watch App/WearablesMiniApp_JonathanKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WearablesMiniApp_JonathanKApp.swift
+//  WearablesMiniApp-JonathanK Watch App
+//
+//  Created by Jonathan Kearns on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WearablesMiniApp_JonathanK_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
